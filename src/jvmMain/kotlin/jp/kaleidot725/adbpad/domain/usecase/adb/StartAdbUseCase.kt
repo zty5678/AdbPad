f6b546d67dfe00c923226059e1a6913f2bfb52c2
zty5678 <zty5678@gmail.com>
2023-05-27 16:42:30 +0800

    fix adb check: use ```StartAdbInteractor().execute()``` to check adb, the user may have set up the adb environment already.

--- src/jvmMain/kotlin/jp/kaleidot725/adbpad/domain/usecase/adb/StartAdbUseCase.kt
+++ src/jvmMain/kotlin/jp/kaleidot725/adbpad/domain/usecase/adb/StartAdbUseCase.kt
@@ -1,7 +1,9 @@
 package jp.kaleidot725.adbpad.domain.usecase.adb
 
+import com.malinskiy.adam.AndroidDebugBridgeClientFactory
 import com.malinskiy.adam.interactor.StartAdbInteractor
 import jp.kaleidot725.adbpad.domain.repository.SettingRepository
+import jp.kaleidot725.adbpad.utils.LogUtils
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.File
@@ -11,11 +13,15 @@ class StartAdbUseCase(
 ) {
     suspend operator fun invoke(): Boolean {
         return withContext(Dispatchers.IO) {
-            val sdkPath = settingRepository.getSdkPath()
-            return@withContext StartAdbInteractor().execute(
-                adbBinary = File(sdkPath.adbDirectory),
-                serverPort = sdkPath.adbServerPort
-            )
+            if (!StartAdbInteractor().execute()) {
+                val sdkPath = settingRepository.getSdkPath()
+                return@withContext StartAdbInteractor().execute(
+                    adbBinary = File(sdkPath.adbDirectory),
+                    serverPort = sdkPath.adbServerPort
+                )
+            }
+
+            true
         }
     }
 }
